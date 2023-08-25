6b8e14ca2b7ac96e9d63068dba69c68bdbe5b8c8
Vo Phong <phongwind21092001@gmail.com>
2023-08-25 13:52:52 +0700

    try Angular Lifecycle hooks - part2

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,11 +1,12 @@
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { AfterContentInit, Component, ContentChild, DoCheck, ElementRef, Input, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.scss']
 })
-export class ChildComponent implements OnChanges, OnInit {
+export class ChildComponent implements OnChanges, OnInit, DoCheck {
+  //@Input() : cho phép truyền dữ liệu từ component cha xuống component con.
   @Input() userName = '';
 
   //ngOnChanges: được gọi lần đầu khi Component được tạo ra và được gọi lại mỗi lần có thay đổi trong Input()
@@ -20,5 +21,11 @@ export class ChildComponent implements OnChanges, OnInit {
     console.log('ngOnInit from the child component');
   }
 
-  //ngDoCheck(): 
+  //ngDoCheck(): được gọi mỗi lần Component có thay đổi.
+  //DoCheck có thể phát hiện được cả những thay đổi mà OnChanges không phát hiện được. 
+  //Angular theo dõi tham chiếu đối tượng, nếu thay đổi đối tượng mà không thay đổi tham chiếu thì Angular sẽ
+  //không phát hiện thay đổi, nhưng DoCheck thì có.
+  ngDoCheck() {
+    console.log('ngDoCheck triggered');
+  }
 }
